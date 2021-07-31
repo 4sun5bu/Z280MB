142ea6eb04cd18ec8466f99d55d6d0da6fe83c33
4sun5bu <kkgwlife@gmai.com>
2021-07-31 18:52:13 +0900

    Refactor bin2ihx.c

--- z280mon/bin2ihx.c
+++ z280mon/bin2ihx.c
@@ -1,8 +1,14 @@
+/******************************************************************************
+ bin2ihx.c
+  make HEX file from binary file with spliting even/odd address data 
+
+  Copyright (c) 2021 4sun5bu 
+******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sysexits.h>
 #include <string.h>
-
 
 #define MAXBC  16
 
@@ -11,14 +17,19 @@ int main(int argc, char *argv[])
     FILE *fin;
     unsigned int addr;
     unsigned char sum;
-    int order;
+    int selct;
+ 
+    int nbytes;
+    int bcnt;
+    int laddr;
+    unsigned char buf[MAXBC];
 
     if (argc < 2) {
         fprintf(stderr, "usage : b2ihx [option] input-file\n");
         exit(EX_USAGE);
     }
 
-    order = -1;
+    selct = -1;
     if (argc == 2)
     {
         fin = fopen(argv[1], "rb");
@@ -26,49 +37,46 @@ int main(int argc, char *argv[])
     if (argc == 3)
     {
         if (strcmp(argv[1], "-odd") == 0)
-            order = 1;
+            selct = 1;
         else if (strcmp(argv[1], "-even") == 0)
-            order = 0;
+            selct = 0;
         fin = fopen(argv[2], "rb");
     }   
     if (fin == NULL)
     {
-        fprintf(stderr, "can not open file\n");
+        fprintf(stderr, "Can not open input file\n");
         exit(EX_USAGE);
     }
-    
-    int n;
-    int bc;
-    int oc;
-    unsigned char buf[MAXBC];
-    
+       
     addr = 0;
-    oc = 0;
+    laddr = 0;
     while (1) {
-        n = fread(buf, 1, MAXBC, fin);
-        if (n == 0)
+        nbytes = fread(buf, 1, MAXBC, fin);
+        if (nbytes == 0)
             break;
 
-        if (order == 0)
-            bc = (n + 1) / 2;
-        else if (order == 1)
-            bc = n / 2;
+        if (selct == 0)
+            bcnt = (nbytes + 1) / 2;
+        else if (selct == 1)
+            bcnt = nbytes / 2;
         else
-            bc = n;
-        printf(":%02x%04x00", bc, oc);
-        sum = bc + (oc & 0xff) + (oc >> 8);
-        for (int i = 0; i < n; i++, addr++) {
-            if ((order == 1) && ((addr % 2) == 0))
+            bcnt = nbytes;
+        printf(":%02x%04x00", bcnt, laddr);
+        
+        sum = bcnt + (laddr & 0xff) + (laddr >> 8);
+        for (int i = 0; i < nbytes; i++, addr++) {
+            if ((selct == 1) && ((addr % 2) == 0))
                 continue;
-            else if ((order == 0) && ((addr % 2) == 1))
+            else if ((selct == 0) && ((addr % 2) == 1))
                 continue;
             printf("%02x", buf[i]);
-            oc++;
+            laddr++;
             sum += buf[i];
         }
+        
         sum = ~sum + 1;
         printf("%02x\n", sum);
-        if (n < MAXBC)
+        if (nbytes < MAXBC)
             break;
     }
     printf(":00000001FF\n");
